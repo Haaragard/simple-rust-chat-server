442cde764b9ff1493ab3d7c5a6a9c56b8673fe83
haaragard <haaragard@gmail.com>
2025-06-26 00:18:06 -0300

    feat: adicionar enum e estrutura para tipos de conteúdo HTTP

--- src/http/enums.rs
+++ src/http/enums.rs
@@ -12,3 +12,62 @@ impl HttpMethodEnum {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct HttpContentFormData {
+    boundary_start: String,
+    boundary_end: String,
+}
+
+impl HttpContentFormData {
+    pub fn from(boundary: String) -> Self {
+        // TODO
+        Self {
+            boundary_start: String::new(),
+            boundary_end: String::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum HttpContentTypeRawEnum {
+    Text,
+    Json,
+}
+
+#[derive(Debug)]
+pub enum HttpContentTypeEnum {
+    None,
+    FormData(HttpContentFormData),
+    Raw(HttpContentTypeRawEnum),
+}
+
+impl HttpContentTypeEnum {
+    pub fn from(content_type: String) -> Self {
+        let split_content = content_type
+            .split(";")
+            .collect::<Vec<&str>>();
+
+        let string_type = split_content
+            .first()
+            .unwrap()
+            .as_ref();
+        match string_type {
+            "multipart/form-data" => {
+                let mut form_data: HttpContentFormData = HttpContentFormData::from(String::from(""));
+
+                let form_data_option = split_content.get(1);
+                if form_data_option.is_some() {
+                    form_data = HttpContentFormData::from(
+                        form_data_option.unwrap().to_string()
+                    );
+                }
+
+                Self::FormData(form_data)
+            },
+            "text/plain" => Self::Raw(HttpContentTypeRawEnum::Text),
+            "application/json" => Self::Raw(HttpContentTypeRawEnum::Json),
+            _ => Self::None,
+        }
+    }
+}
