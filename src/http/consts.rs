575ed7547170d0de2a8143cc07025da338f04038
haaragard <haaragard@gmail.com>
2025-06-26 00:17:52 -0300

    feat: adicionar constantes para CRLF e parâmetros de consulta

--- src/http/consts.rs
+++ src/http/consts.rs
@@ -1,1 +1,5 @@
 pub const CRLF: &'static str = "\r\n";
+pub const DOUBLE_CRLF: &'static str = "\r\n\r\n";
+
+pub const QUERY_PARAM_START: &'static str = "?";
+pub const QUERY_PARAM_SEPARATOR: &'static str = "&";
