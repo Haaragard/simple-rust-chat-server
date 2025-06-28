c2dcf0564c33484d75f5cd08a8a2f7b785c10bcf
haaragard <haaragard@gmail.com>
2025-06-28 19:33:56 -0300

    feat: adicionar constantes para métodos HTTP GET e POST

--- src/http/consts.rs
+++ src/http/consts.rs
@@ -3,3 +3,6 @@ pub const DOUBLE_CRLF: &'static str = "\r\n\r\n";
 
 pub const QUERY_PARAM_START: &'static str = "?";
 pub const QUERY_PARAM_SEPARATOR: &'static str = "&";
+
+pub const HTTP_METHOD_GET: &'static str = "GET";
+pub const HTTP_METHOD_POST: &'static str = "POST";
